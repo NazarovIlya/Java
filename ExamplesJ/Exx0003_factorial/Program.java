acd6fd6735c05ddf00a32b1a9c03fe3bc9bce56c
NazarovIlya <eliakohai@gmail.com>
2022-11-22 19:24:39 +0300

    factorial

--- ExamplesJ/Exx0003_factorial/Program.java
+++ ExamplesJ/Exx0003_factorial/Program.java
@@ -5,17 +5,22 @@ public class Program {
         System.out.println("Enter the intger:\t\n");
         Scanner iScanner = new Scanner(System.in);
         int num = iScanner.nextInt();
-        int result = MFac.Factorial(num);
+        double result = Factorial(num);
+        iScanner.close();
         System.out.printf("Factorial of %d: %d", num, result);
     }
 
-    public static int Factorial(int number) {
+    public static double Factorial(int number) {
         if (number == 1)
             return 1;
         return number * Factorial(number - 1);
     }
 }
 
+
+    
+        
+    
 
     
         
