5ddd3decf4de212e857fb8752d37f326050de87b
Nazarov Ilya <108418489+NazarovIlya@users.noreply.github.com>
2022-11-25 01:36:39 +0300

    Delete TasksJ/Task005_ directory

--- TasksJ/Task005_/Program.java
+++
@@ -1,9 +0,0 @@
-// Задание №5 (доп)
-// Во фразе "Добро пожаловать на курс по Java" 
-// переставить слова в обратном порядке.
-
-public class Program {
-    public static void main(String[] args) {
-
-    
-}
