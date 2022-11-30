aa2e5eed4531b2208b9044522688051fab5d5966
NazarovIlya <eliakohai@gmail.com>
2022-11-30 17:02:54 +0300

    min & max & average in the ArrayList initial

---
+++ TasksJ/Task030_MinMaxAverageInArrayList/Program.java
@@ -0,0 +1,9 @@
+// ДЗ 3.3
+// Задан целочисленный список ArrayList. 
+// Найти минимальное, максимальное и среднее из этого списка.
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
