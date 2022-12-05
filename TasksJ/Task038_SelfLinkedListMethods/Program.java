8906aaf1e7ee9bc03ee8828a5af9b408361729fc
NazarovIlya <eliakohai@gmail.com>
2022-12-06 01:25:03 +0300

    in process...

---
+++ TasksJ/Task038_SelfLinkedListMethods/Program.java
@@ -0,0 +1,46 @@
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди, 
+// dequeue() - возвращает первый элемент из очереди и удаляет его, 
+// first() - возвращает первый элемент из очереди, не удаляя.
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Введите количество элементов списка.");
+        int size = iScanner.nextInt();
+        iScanner.close();
+        LinkedList<Integer> linkedList = CreateNFillLinkedList(size);
+        System.out.printf("Исходный список: %s\n", linkedList);
+        int res = Dequeue(linkedList);
+        System.out.printf("Конечный список: %s,\nПервый элемент списка, который был удален %s\n", linkedList, res);
+
+    }
+
+    public static LinkedList<Integer> CreateNFillLinkedList(int size) {
+        Random rnd = new Random();
+        LinkedList<Integer> lnkList = new LinkedList<>();
+        for (int i = 0; i < size; i++) {
+            lnkList.add(rnd.nextInt(0, 100));
+        }
+        return lnkList;
+    }
+
+    public static void Enqueue(LinkedList<Integer> queue, int element) {
+        queue.addLast(element);
+    }
+
+    public static int Dequeue(LinkedList<Integer> queue) {
+        int firstElement = queue.poll();
+        return firstElement;
+    }
+
+    public static int First(LinkedList<Integer> queue) {
+        int first = queue.getFirst();
+        return first;
+    }
+}
