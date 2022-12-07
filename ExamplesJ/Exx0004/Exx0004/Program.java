f51473e3a6b2e177c6e8168b7c096e54896585a9
NazarovIlya <eliakohai@gmail.com>
2022-12-07 22:29:28 +0300

    initial

---
+++ ExamplesJ/Exx0004/Exx0004/Program.java
@@ -0,0 +1,8 @@
+package Exx0004;
+
+public class Program {
+    public static void main(String[] args) {
+        Worker w1 = new Worker(1);
+        System.out.println(w1.toString());
+    }
+}
---
+++ ExamplesJ/Exx0004/Exx0004/Worker.java
@@ -0,0 +1,22 @@
+package Exx0004;
+
+public class Worker {
+    int ID;
+    // int Salary;
+    // String FirstName;
+    // String Family;
+    // String LastName;
+
+    public Worker(int id) {
+        ID = id;
+        // Salary = salary;
+        // FirstName = firstName;
+        // Family = family;
+        // LastName = lastNameS;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s", ID);
+    }
+}
