f7e050c802bed7f1394ee634dfc61f46a23084b0
Nazarov Ilya <108418489+NazarovIlya@users.noreply.github.com>
2022-12-07 22:27:49 +0300

    Delete ExamplesJ/Exx0004 directory

--- ExamplesJ/Exx0004/Program.java
+++
@@ -1,7 +0,0 @@
-package Ex0004;
-
-public class Program {
-    public static void main(String[] args) {
-        Worker w = new Worker();
-    }
-}
--- ExamplesJ/Exx0004/Worker.java
+++
@@ -1,9 +0,0 @@
-package Ex0004;
-
-public class Worker {
-    int ID;
-
-    public Worker(int id) {
-        ID = id;
-    }
-}
