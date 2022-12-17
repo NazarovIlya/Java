4097594fed8f00f81af5dd644db9ec873dc84ee5
NazarovIlya <eliakohai@gmail.com>
2022-12-18 00:46:05 +0300

    problem in Human class

--- OOP_J/Tasks/Proj001_FamilyTree/FamilyTree/Human.java
+++ OOP_J/Tasks/Proj001_FamilyTree/FamilyTree/Human.java
@@ -1,33 +1,50 @@
 package FamilyTree;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Human {
     private String name;
     private int age;
     private String sex;
-    // private Random random;
+    private Human father;
+    private List<Human> children;
 
-    public Human(String name, int age, String sex) {
+    public Human(String name, int age, String sex, Human father) {
         this.name = name;
         this.age = age;
         this.sex = sex;
+        this.father = father;
+        this.children = new ArrayList<>();
+        father.getChildren().add(this);
+    }
+
+    public Human(String name, Human father) {
+        this.name = name;
+        this.age = 0;
+        this.sex = getRandomSex();
+        this.father = father;
+        this.children = new ArrayList<>();
+        father.getChildren().add(this);
     }
 
     public Human(String name) {
         this.name = name;
-        this.age = 0;
-        this.sex = getRandomSex();
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public List<Human> getChildren() {
+        return this.children = new ArrayList<>();
+    }
+
     @Override
     public String toString() {
-        return String.format("name: %s\nage: %s\nsex: %s\n",
-                this.name, this.age, this.sex);
+        return String.format("name: %s, age: %s, sex: %s\nFather: %s\nChildren: %s",
+                this.name, this.age, this.sex, this.father, this.children.toString());
     }
 
     private String getRandomSex() {
--- OOP_J/Tasks/Proj001_FamilyTree/Main.java
+++
@@ -1,8 +0,0 @@
-import FamilyTree.Human;
-
-public class Main {
-    public static void main(String[] args) {
-        Human human = new Human("Лунтик");
-        System.out.println(human.toString());
-    }
-}
