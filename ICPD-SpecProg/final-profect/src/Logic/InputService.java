1332ef66c691f51ec6d4ed2b523dd20b0a1ebc43
NazarovIlya <eliakohai@gmail.com>
2024-10-27 15:38:32 +0300

    mvp input completed

--- ICPD-SpecProg/final-profect/src/Logic/InputService.java
+++ ICPD-SpecProg/final-profect/src/Logic/InputService.java
@@ -1,21 +1,50 @@
 package Logic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class InputService implements IReadable{
     private String filePath;
     private String fileName;
+    private Scanner scanner;
+    private int index;
 
     public InputService(String filePath, String fileName) {
         this.filePath = filePath;
         this.fileName = fileName;
+        try {
+            this.scanner = new Scanner(new File(String.format("%s%s", filePath, fileName)));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        this.index = 0;
     }
 
     @Override
     public void ReadFromTXT() {
-        Scanner scanner = new Scanner(String.format("%s%s", filePath, fileName));
+        String str = "";
         while (scanner.hasNext()){
-            break;
+            if(scanner.hasNextLine()){
+                int[] arr = new int[]{0, 0};
+                str = scanner.nextLine();
+                String[] arrStr = new String[]{"", ""};
+                if(str.contains("-")){
+                    int first = (int)arr[0];
+                    int second = (int)arr[0];
+                    System.out.println(String.format("%d --> %d-%d", ++index, first, second));
+                } else
+                System.out.println(str);
+                System.out.println(String.format("%d --> %s", ++index , str));
+            } else if (scanner.hasNextInt()) {
+                int num = scanner.nextInt();
+                System.out.println(String.format("%d --> %d", ++index, num));
+            } else if (scanner.hasNextFloat()) {
+                float num = scanner.nextFloat();
+                System.out.println(String.format("%d --> %f", ++index, num));
+            }
+
+//            break;
         }
     }
 }
