7db3d1a4d81fb7676f6022475005ca81817b28ed
NazarovIlya <eliakohai@gmail.com>
2024-10-27 22:47:05 +0300

    in process

--- ICPD-SpecProg/final-profect/src/Presenter.java
+++ ICPD-SpecProg/final-profect/src/Presenter.java
@@ -23,28 +23,34 @@ public class Presenter {
         IView view = new ViewConsole();
         InputService inputService = new InputService(".\\resources\\", "data_univer.txt");
         ArrayList<UniversityModel> universityList = new ArrayList<UniversityModel>();
-        UniversityService service = new UniversityService(universityList);
+//        UniversityService service = new UniversityService(universityList);
 
-        UniversityModel a = new UniversityModel();
-        UniversityModel b = new UniversityModel();
-
-        a.setName("BBB");
-        a.setScores(120, 160);
-        b.setName("AAA");
-
-        universityList.add(a);
-        universityList.add(b);
-
-
-        System.out.println(universityList);
-//      САМАЯ ПЕРВАЯ СОРТИРОВКА - нужна в любом случае для того, чтобы не вызывать методы лишний раз
-        service.sortByNameAZ();
-        System.out.println(universityList);
-
+//        UniversityModel a = new UniversityModel();
+//        UniversityModel b = new UniversityModel();
+//
+//        a.setName("BBB");
+//        a.setScores(120, 160);
+//        b.setName("AAA");
+//
+//        universityList.add(a);
+//        universityList.add(b);
+//
+//
+//        System.out.println(universityList);
+////      САМАЯ ПЕРВАЯ СОРТИРОВКА - нужна в любом случае для того, чтобы не вызывать методы лишний раз
+//        service.sortByNameAZ();
+//        System.out.println(universityList);
+        ArrayList<UniversityModel> universityModels = inputService.ReadFromTXT();
+        System.out.println(universityModels);
 
         int i = 0;
         while (true) {
-            ArrayList<UniversityModel> universityModels= inputService.ReadFromTXT();
+
+            UniversityService service = new UniversityService(universityModels);
+
+            //  САМАЯ ПЕРВАЯ СОРТИРОВКА - нужна в любом случае для того, чтобы не вызывать методы лишний раз
+            service.sortByNameAZ();
+
             view.menu(commands);
 
             System.out.println(universityModels);
